3da6a6fa410ddd1601017736d49d208993c6fe24
Oleksandr Korniienko <k0.orne@gmail.com>
2025-05-11 23:04:23 +0100

    ver_1.1_29.04.25

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("RABOTAET SUKA");
+console.log("STARTOF THE SCRIPT!!!");
     const currentCity = "amsterdam";
     const title = document.getElementById("title");
-    // title.innerHTML = currentCity + " weather forecast"; //ERROR
-    console.log("TYT");
+    try {title.innerHTML = currentCity + " weather forecast";} catch (e) {console.log("ERROR title")} //ERROR
+    console.log("POINTER FOR THE ERROR <======================="); // <<<<<===============================================
     const currentCityData = weatherData[currentCity + "_daily"] // amsterdam_daily
     const currentCityDataHourly = weatherData[currentCity + "_hourly"] // amsterdam_daily
     const day = document.getElementById("day");               // <span id="day">Today</span>
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentTime = document.getElementById("currentTime");
     const currentTimeHours = new Date().getHours();
     const currentTimeMinutes = new Date().getMinutes();
-    currentTime.innerHTML = currentTimeHours + ":" + currentTimeMinutes;
+    try {currentTime.innerHTML = currentTimeHours + ":" + currentTimeMinutes;} catch (e) {console.log("ERROR currentTime")}
     
 
     function hoursToTwoChars() {
@@ -52,42 +52,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const indexDaily = 0;
     const indexHourly = 0;
     // day.innerHTML = currentCityData.daily.day[0]; //error
-    cityName.innerHTML = currentCity;
-
-    daily_weather_code.innerHTML = currentCityData.daily.weather_code[indexDaily];
-    daily_temperature_2m_max.innerHTML = currentCityData.daily.temperature_2m_max[indexDaily] + currentCityData.daily_units.temperature_2m_max;
-    daily_temperature_2m_min.innerHTML = currentCityData.daily.temperature_2m_min[indexDaily] + currentCityData.daily_units.temperature_2m_min;
-    daily_apparent_temperature_max.innerHTML = currentCityData.daily.apparent_temperature_max[indexDaily] + currentCityData.daily_units.apparent_temperature_max;
-    daily_apparent_temperature_min.innerHTML = currentCityData.daily.apparent_temperature_min[indexDaily] + currentCityData.daily_units.apparent_temperature_min;
-    daily_sunrise.innerHTML = currentCityData.daily.sunrise[indexDaily] + currentCityData.daily_units.sunrise;
-    daily_sunset.innerHTML = currentCityData.daily.sunset[indexDaily] + currentCityData.daily_units.sunset;
-    daily_precipitation_hours.innerHTML = currentCityData.daily.precipitation_hours[indexDaily] + currentCityData.daily_units.precipitation_hours;
-    daily_precipitation_probability_max.innerHTML = currentCityData.daily.precipitation_probability_max[indexDaily] + currentCityData.daily_units.precipitation_probability_max;
-    daily_wind_speed_10m_max.innerHTML = currentCityData.daily.wind_speed_10m_max[indexDaily] + currentCityData.daily_units.wind_speed_10m_max;
-    daily_wind_gusts_10m_max.innerHTML = currentCityData.daily.wind_gusts_10m_max[indexDaily] + currentCityData.daily_units.wind_gusts_10m_max;
-    daily_wind_direction_10m_dominant.innerHTML = currentCityData.daily.wind_direction_10m_dominant[indexDaily] + currentCityData.daily_units.wind_direction_10m_dominant;
+    try {cityName.innerHTML = currentCity;}catch (e) {console.log("ERROR cityName")}
+    
+    try {daily_weather_code.innerHTML = currentCityData.daily.weather_code[indexDaily];} catch (e) {console.log("ERROR daily_weather_code")}
+    try {daily_temperature_2m_max.innerHTML = currentCityData.daily.temperature_2m_max[indexDaily] + currentCityData.daily_units.temperature_2m_max;} catch (e) {console.log("ERROR daily_temperature_2m_max")}
+    try {daily_temperature_2m_min.innerHTML = currentCityData.daily.temperature_2m_min[indexDaily] + currentCityData.daily_units.temperature_2m_min;} catch (e) {console.log("ERROR daily_temperature_2m_min")}
+    try {daily_apparent_temperature_max.innerHTML = currentCityData.daily.apparent_temperature_max[indexDaily] + currentCityData.daily_units.apparent_temperature_max;} catch (e) {console.log("ERROR daily_apparent_temperature_max")}
+    try {daily_apparent_temperature_min.innerHTML = currentCityData.daily.apparent_temperature_min[indexDaily] + currentCityData.daily_units.apparent_temperature_min;} catch (e) {console.log("ERROR daily_apparent_temperature_min")}
+    try {daily_sunrise.innerHTML = currentCityData.daily.sunrise[indexDaily] + currentCityData.daily_units.sunrise;} catch (e) {console.log("ERROR daily_sunrise")}
+    try {daily_sunset.innerHTML = currentCityData.daily.sunset[indexDaily] + currentCityData.daily_units.sunset;} catch (e) {console.log("ERROR daily_sunset")}
+    try {daily_precipitation_hours.innerHTML = currentCityData.daily.precipitation_hours[indexDaily] + currentCityData.daily_units.precipitation_hours;} catch (e) {console.log("ERROR daily_precipitation_hours")}
+    try {daily_precipitation_probability_max.innerHTML = currentCityData.daily.precipitation_probability_max[indexDaily] + currentCityData.daily_units.precipitation_probability_max;} catch (e) {console.log("ERROR daily_precipitation_probability_max")}
+    try {daily_wind_speed_10m_max.innerHTML = currentCityData.daily.wind_speed_10m_max[indexDaily] + currentCityData.daily_units.wind_speed_10m_max;}   catch (e) {console.log("ERROR daily_wind_speed_10m_max")}
+    try {daily_wind_gusts_10m_max.innerHTML = currentCityData.daily.wind_gusts_10m_max[indexDaily] + currentCityData.daily_units.wind_gusts_10m_max;} catch (e) {console.log("ERROR daily_wind_gusts_10m_max")}
+    try {daily_wind_direction_10m_dominant.innerHTML = currentCityData.daily.wind_direction_10m_dominant[indexDaily] + currentCityData.daily_units.wind_direction_10m_dominant;}    catch (e) {console.log("ERROR daily_wind_direction_10m_dominant")}
     
 
     //=============================hourly============================================
 
-    hourly_temperature_2m.innerHTML = currentCityDataHourly.hourly.temperature_2m[indexHourly];
-    hourly_relative_humidity_2m.innerHTML = currentCityDataHourly.hourly.relative_humidity_2m[indexHourly];
-    hourly_apparent_temperature.innerHTML = currentCityDataHourly.hourly.apparent_temperature[indexHourly];
-    hourly_precipitation_probability.innerHTML = currentCityDataHourly.hourly.precipitation_probability[indexHourly];
-    hourly_precipitation.innerHTML = currentCityDataHourly.hourly.precipitation[indexHourly];
-    hourly_weather_code.innerHTML = currentCityDataHourly.hourly.weather_code[indexHourly];
-    hourly_wind_speed_10m.innerHTML = currentCityDataHourly.hourly.wind_speed_10m[indexHourly];
-    hourly_wind_gusts_10m.innerHTML = currentCityDataHourly.hourly.wind_gusts_10m[indexHourly];
+    try {hourly_temperature_2m.innerHTML = currentCityDataHourly.hourly.temperature_2m[indexHourly];}   catch (e) {console.log("ERROR hourly_temperature_2m")}
+    try {hourly_relative_humidity_2m.innerHTML = currentCityDataHourly.hourly.relative_humidity_2m[indexHourly];} catch (e) {console.log("ERROR hourly_relative_humidity_2m")}
+    try {hourly_apparent_temperature.innerHTML = currentCityDataHourly.hourly.apparent_temperature[indexHourly];} catch (e) {console.log("ERROR hourly_apparent_temperature")}
+    try {hourly_precipitation_probability.innerHTML = currentCityDataHourly.hourly.precipitation_probability[indexHourly];} catch (e) {console.log("ERROR hourly_precipitation_probability")} 
+    try {hourly_precipitation.innerHTML = currentCityDataHourly.hourly.precipitation[indexHourly];} catch (e) {console.log("ERROR hourly_precipitation")}
+    try {hourly_weather_code.innerHTML = currentCityDataHourly.hourly.weather_code[indexHourly];} catch (e) {console.log("ERROR hourly_weather_code")} 
+    try {hourly_wind_speed_10m.innerHTML = currentCityDataHourly.hourly.wind_speed_10m[indexHourly];}   catch (e) {console.log("ERROR hourly_wind_speed_10m")}
+    try {hourly_wind_gusts_10m.innerHTML = currentCityDataHourly.hourly.wind_gusts_10m[indexHourly];} catch (e) {console.log("ERROR hourly_wind_gusts_10m")}
 
     
 
-    // maxWind.innerHTML = currentCityData.daily.wind_speed_10m_max[0] + " " + currentCityData.daily_units.wind_speed_10m_max;
+    try {maxWind.innerHTML = currentCityData.daily.wind_speed_10m_max[0] + " " + currentCityData.daily_units.wind_speed_10m_max;} catch (e) {console.log("ERROR maxWind")}
 
     //============================now==============================================
-    // currentTemp.innerHTML = currentCityDataHourly.hourly.temperature_2m[indexForHourly] + " " + currentCityDataHourly.hourly_units.temperature_2m;
-    // currentTempFeel.innerHTML = currentCityDataHourly.hourly.apparent_temperature[indexForHourly] + " " + currentCityDataHourly.hourly_units.apparent_temperature;
-    // currentWind.innerHTML = currentCityDataHourly.hourly.wind_speed_10m[indexForHourly] + " " + currentCityDataHourly.hourly_units.wind_speed_10m;
-    
-    console.log("DO KONCA");
+    try {currentTemp.innerHTML = currentCityDataHourly.hourly.temperature_2m[indexForHourly] + " " + currentCityDataHourly.hourly_units.temperature_2m;} catch (e) {console.log("ERROR currentTemp")}
+    try {currentTempFeel.innerHTML = currentCityDataHourly.hourly.apparent_temperature[indexForHourly] + " " + currentCityDataHourly.hourly_units.apparent_temperature;} catch (e) {console.log("ERROR currentTempFeel")}
+    try {currentWind.innerHTML = currentCityDataHourly.hourly.wind_speed_10m[indexForHourly] + " " + currentCityDataHourly.hourly_units.wind_speed_10m;} catch (e) {console.log("ERROR currentWind")}
+    console.log("END OF THE SCRIPT!!!");
 
 });
---
+++ scripts/test.js
@@ -0,0 +1,4 @@
+console.log("POINTER FOR THE ERROR <======================="); // <<<<<===============================================
+
+const colorMode = document.body.classList.contains('light') ? 'light' : 'dark';
+const state = document.querySelector("#switch").checked ? "dark" : "light";
